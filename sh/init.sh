68e7402657ac6482d8451f4886d34eed08232846
尤雨东 <root@ltpp.vip>
2025-02-07 22:48:07 +0800

    feat:

--- sh/init.sh
+++ sh/init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-git remote add jihulab git@jihulab.com:ltpp-universe/html-pdf.git;
+git remote add gitee git@gitee.com:ltpp-universe/html-pdf.git;
 git remote add github git@github.com:ltpp-universe/HTML-PDF.git;
 git remote add origin git@github.com:ltpp-universe/HTML-PDF.git;
